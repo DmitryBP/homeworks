6d59109d77dad6012a1c1d5cecde8812c1c85b46
Dmitry_home <79811924115@yandex.ru>
2022-02-01 19:42:43 +0300

    task1+task2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -21,18 +21,20 @@ function solveEquation(a, b, c) {
     return nullD
   }git 
 }
-
+  // код для задачи №2 писать здесь
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
- 
-  // код для задачи №2 писать здесь
-  if (typeof percent !== 'number'||
-      typeof contribution !== 'number'||
-      typeof amount !== 'number'||
-      typeof date !== 'number'
-      )
-  return 'not number'
-  // return totalAmount;
+  let date2 = new Date(date)
+  let date1 = new Date()
+  let n = Math.round((date2 - date1)*12/(365*24*3600*1000)) // - количество месяцев
+  let S = amount - contribution // - тело кредита
+  let P = (percent/100)/12
+  let monthPay = S * (P + (P / ( Math.pow((1 + P), n) - 1)))
+  console.log(`Срок кредита - ${n} месяцев`)
+  console.log(`Ежемесячный платеж составит - ${monthPay.toFixed(2)} руб.`)
+  totalAmount = monthPay * n
+  return `Общая сумма которую придется запалатить составит - ${totalAmount.toFixed(2)} руб.`
+
 }
 
-console.log(calculateTotalMortgage(1, 2, 3, 4))
+console.log(calculateTotalMortgage(10, 1000, 50000, '2023.02.01.'))
