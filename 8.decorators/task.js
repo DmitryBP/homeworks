e9775ad9ab0b9097615dbc0f9ed9ea901ecccbdd
Dmitry_home <79811924115@yandex.ru>
2022-03-14 21:53:46 +0300

    1 task complited

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,27 +1,28 @@
 
   function cachingDecoratorNew(func) {
     let cache = [];
-    
+  
     function wrapper(...args) {
+  
+      
         const hash = args.join(','); // получаем правильный хэш +
-        console.log (cache)
-        console.log (hash)
-        // let idx = cache.findIndex((item)=> item.cache = hash ); 
-        // console.log (idx)// ищем элемент, хэш которого равен нашему хэшу
-        // console.log (cache)// ищем элемент, хэш которого равен нашему хэшу
-        // if (idx !== -1 ) { // если элемент не найден
-        //     console.log("Из кэша: " + ???); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-        //     return "Из кэша: " + ???;
-        // }
-    
+        let idx = cache.findIndex((obj)=> obj.hash == hash); // ищем элемент, хэш которого равен нашему хэшу
+  
+        if (idx !== -1 ) { // если элемент не найден
+            console.log("Из кэша: " + cache[idx]['value']); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кэша: " + cache[idx];
+        }
         let result = func(...args); // в кэше результата нет - придётся считать
-        cache.push({hash:result}) ; // добавляем элемент с правильной структурой
-        
-        // if (cache.length > 5) { 
-        //   ??? // если слишком много элементов в кэше надо удалить самый старый (первый) 
-        // }
-        // console.log("Вычисляем: " + result);
-        // return "Вычисляем: " + result;  
+        let cacheItem = {
+          hash: hash,
+          value: result,
+        }
+        cache.push(cacheItem) ; // добавляем элемент с правильной структурой
+        if (cache.length > 5) { 
+          cache.shift(cacheItem) // если слишком много элементов в кэше надо удалить самый старый (первый) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
     }
     return wrapper;
     }
@@ -37,13 +38,6 @@
   upgradedAddThree(5, 2, 3); // вычисляем: 10
   upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
   upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
-
-
-
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
-}
+  
+  
+  
