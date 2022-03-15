8a8af5e619718983ff5b96c81f93369d6e48e4f4
Dmitry_home <79811924115@yandex.ru>
2022-03-15 23:40:44 +0300

    1 and 2 task complited

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -30,14 +30,47 @@
   const addThree = (a, b, c) => a + b + c;
   const upgradedAddThree = cachingDecoratorNew(addThree);
   
-  upgradedAddThree(1, 2, 3); // вычисляем: 6
-  upgradedAddThree(1, 2, 3); // из кэша: 6
-  upgradedAddThree(2, 2, 3); // вычисляем: 7
-  upgradedAddThree(3, 2, 3); // вычисляем: 8
-  upgradedAddThree(4, 2, 3); // вычисляем: 9
-  upgradedAddThree(5, 2, 3); // вычисляем: 10
-  upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
-  upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
+  // upgradedAddThree(1, 2, 3); // вычисляем: 6
+  // upgradedAddThree(1, 2, 3); // из кэша: 6
+  // upgradedAddThree(2, 2, 3); // вычисляем: 7
+  // upgradedAddThree(3, 2, 3); // вычисляем: 8
+  // upgradedAddThree(4, 2, 3); // вычисляем: 9
+  // upgradedAddThree(5, 2, 3); // вычисляем: 10
+  // upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
+  // upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
   
   
-  
+  //Задача 2 
+
+
+const sendSignal = () => console.log("Сигнал отправлен"); // исходная функция
+
+function debounceDecoratorNew(f, ms) { 
+    let timeout;
+    let flag = false;
+    function wrapper (...args) {
+      if(!flag) {
+        f(...args)
+        flag = true;
+      }
+      else {
+        clearTimeout(timeout)
+        timeout = setTimeout(()=>f(...args), ms)
+      }
+    } 
+    return wrapper;
+}
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(upgradedSendSignal); // Сигнал отправлен
+setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
+setTimeout(upgradedSendSignal, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
+setTimeout(upgradedSendSignal, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (300 < 2000)
+setTimeout(upgradedSendSignal, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
+setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
+setTimeout(upgradedSendSignal, 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
+
+
+// Подсказка 1
+// Вызывайте переданную функцию немедленно и взводите флаг.
+// Подсказка 2
+// Используйте setTimeout для снятия флага и при каждом вызове проверяйте, взведен ли он.
